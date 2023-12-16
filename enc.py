ad16c28001a5d29f7f66f0fd1631e20cfcf99a5a
ALi Koja <92184267+K0J4@users.noreply.github.com>
2023-12-16 20:37:32 +0400

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,4 +1,5 @@
 import os
-os.system('cd && rm -rf Encryption')
-try:os.system('chmod 777 enc && ./enc')
-except:import enc
+try:
+  os.system('chmod 777 enc && ./enc')
+except:
+  from enc import main
